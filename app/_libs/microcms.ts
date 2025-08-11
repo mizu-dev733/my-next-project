a985f3179855b58c937680f0d37e5f9bdd0d7a45
mizu-dev733 <kenta733work@gmail.com>
2025-08-12 06:02:16 +0900

    3章まで完了

---
+++ app/_libs/microcms.ts
@@ -0,0 +1,15 @@
+export type Category = {
+  name: string;
+};
+
+export type News = {
+  id: string;
+  title: string;
+  category: {
+    name: string;
+  };
+  publishedAt: string;
+  createdAt: string;
+};
+
+
