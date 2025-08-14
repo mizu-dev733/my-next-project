4b40c7aa3ebb4e313df23b046177a7ace3d74d25
mizu-dev733 <kenta733work@gmail.com>
2025-08-15 08:42:18 +0900

    9章まで完了

---
+++ app/contact/layout.tsx
@@ -0,0 +1,17 @@
+import Hero from "@/app/_components/Hero";
+import Sheet from "../_components/Sheet";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function RootLayout({ children }: Props) {
+  return (
+    <>
+      <Hero title="Contact" sub="お問い合わせ" />
+      <Sheet>
+        {children}
+      </Sheet>
+    </>
+  )
+}
